1ea956e7160b2061dc73c55b8154987a2474031e
envy <2611010122@undc.edu.pe>
2026-05-02 17:36:06 -0500

    update

--- minor-motion/astro.config.mjs
+++ minor-motion/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://envysinho.github.io",
-  base: "/fiundc-landing",
+  base: "/fiundc-landing/",
 });
